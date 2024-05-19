e15d3ca937063e422845eaa44b4611843481b8bc
Dharmi1236014 <161835237+Dharmi1236014@users.noreply.github.com>
2024-05-19 23:10:44 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/* @type {import('tailwindcss').Config} */
+tailwind.config = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+    
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        dharmi:['Dancing Script','cursive'],
+      },
+      extend: {
+        
+      }
+    },
+  }
